9d923242caf832e6187beeb470d26634570227df
nishantkw <133154182+nishantkw@users.noreply.github.com>
2024-04-25 23:33:18 +0530

    Create leftmostandrightmostindexofanarray.cpp

---
+++ leftmostandrightmostindexofanarray.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+        int a=-1,b=-1;
+     
+        for(int i=0;i<v.size();i++){
+            if(v[i]==x) a=i;
+            
+        }
+        for(int i=v.size()-1;i>=0;i--){
+            if(v[i]==x) b=i;
+            
+        }
+        return {b,a};
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+
+// } Driver Code Ends
